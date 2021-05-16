04d19fa4207f180a750f63443d519e550d7aa596
Anurag tyagi <anuragtyagi@Anurags-MacBook-Air.local>
2021-05-16 21:23:42 +0530

    Update 3.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "827ea55e6969c27f12fb62478948b63f",
-"index.html": "d4bde8d2c0357c7ef56c1f0c28ff43c0",
-"/": "d4bde8d2c0357c7ef56c1f0c28ff43c0",
-"main.dart.js": "d2a46d21bba943ad56940311b888b7f5",
+"index.html": "80100b65979228623befe9904958eaf8",
+"/": "80100b65979228623befe9904958eaf8",
+"main.dart.js": "7f3cc39b110a2d30eb8a79af9a8b0280",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
